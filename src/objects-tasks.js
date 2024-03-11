2ed0b17f8b0302c516548b94e244f9e007084ce7
AlexeiMo <prestigio458@gmail.com>
2024-03-11 20:45:56 +0300

    feat: solve 7th task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -136,8 +136,24 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  let wordLength = 0;
+  const entries = Object.entries(lettersObject);
+  for (let i = 0; i < entries.length; i += 1) {
+    wordLength += entries[i][1].length;
+  }
+
+  const word = new Array(wordLength);
+
+  for (let i = 0; i < entries.length; i += 1) {
+    for (let j = 0; j < entries[i][1].length; j += 1) {
+      const index = +entries[i][1][j];
+      const value = entries[i][0];
+      word[index] = value;
+    }
+  }
+
+  return word.join('');
 }
 
 /**
